1f89165d8e95e0df8222e978727a74abd1d21e6a
a12027638 <a12027638@Cinza06>
2014-11-26 10:55:38 -0200

    6

--- src/main/java/io/github/danielfs/calculadora/operacao/binaria/AdicaoIncrementada.java
+++ src/main/java/io/github/danielfs/calculadora/operacao/binaria/AdicaoIncrementada.java
@@ -12,7 +12,7 @@ public class AdicaoIncrementada implements OperacaoBinaria{
 
     public double calcular(double operando1, double operando2) {
         
-        return 0;
+        return 6;
     }
     
 }
